3266ac3fac4289c614c8219e3b28ed6c9a07452d
Yura Horak <yhorak@blackthorn-vision.com>
2016-02-07 21:11:57 +0200

    Project renamed from CsvUtil to the jUtils

--- CsvUtil/Properties/AssemblyInfo.cs
+++ CsvUtil/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CsvUtil")]
+[assembly: AssemblyTitle("jUtils")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CsvUtil")]
+[assembly: AssemblyProduct("jUtils")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
